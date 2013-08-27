43ef62ae90d06a26d8ae138f4b2e075d12d3dd02
Androo <loneleeandro.o@gmail.com>
2013-08-27 13:56:42 +1000

    Added ngMeteor Template

--- modules/ngMeteor.template.js
+++ modules/ngMeteor.template.js
@@ -1,2 +1,10 @@
 var ngMeteorTemplate = angular.module('ngMeteor.template', []);
 
+ngMeteorTemplate.run(['$templateCache', '$rootScope', '$compile', 
+	function($templateCache, $rootScope, $compile) {
+		for(key in Template){
+			render = Template[key];
+			$templateCache.put(key + ".html", render() );
+		}
+	}
+]);
