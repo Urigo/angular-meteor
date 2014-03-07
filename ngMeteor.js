5331ffbb1ee29ef687d35196a57c3e69329f57c5
Androo <loneleeandro.o@gmail.com>
2014-03-07 11:52:38 +1100

    Working on iron-router compatibility

--- ngMeteor.js
+++ ngMeteor.js
@@ -31,3 +31,33 @@ angular.element(document).ready(function() {
 		angular.bootstrap(document, ['ngMeteor']);
 	}
 });
+
+
+/*
+// Manual initilisation of ngMeteor
+angular.element(document).ready(function() {
+	if (!angular.element(document).injector()){
+		angular.bootstrap(document, ['ngMeteor']);
+	}
+	angular.forEach(Template, function(template,name){
+		if(name != 'layout' && name != '__defaultLayout__'){
+			template.rendered = function(){
+				//var html = $(this.data.yield().string);
+
+				html = $('body');
+				console.log( name );
+				console.log( angular.element(html).scope() );
+
+				if (!angular.element(document).injector()){
+					angular.bootstrap(document, ['ngMeteor']);
+				}else{
+					angular.element(document).injector().invoke(function($rootScope, $compile, $document){
+						$compile($document)($rootScope);
+						$rootScope.$digest();
+					});
+				}
+			}
+		}
+	});
+});
+*/
