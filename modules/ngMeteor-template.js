c63fbe6027c07caf550af9aa7f62942ca3a28c12
Androo <loneleeandro.o@gmail.com>
2014-03-11 22:30:44 +1100

    Update ngMeteor-template.js

--- modules/ngMeteor-template.js
+++ modules/ngMeteor-template.js
@@ -3,7 +3,7 @@ var ngMeteorTemplate = angular.module('ngMeteor.template', []);
 ngMeteorTemplate.run(['$templateCache',
 	function($templateCache) {
 		angular.forEach(Template, function(render, name){
-			if(name !== "__define__"){
+			if(name.charAt(0) != "_"){
 				$templateCache.put(name, render() );
 			}
 		});
