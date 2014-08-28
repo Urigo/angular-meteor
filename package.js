4b286e8c6c82f6611c6e6afa2558bb4aa6140c24
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-08-28 19:06:57 +0200

    New version for Meteor packaging system

--- package.js
+++ package.js
@@ -1,29 +1,38 @@
 Package.describe({
-	summary: "The simplest no-conflict way to use AngularJS with Meteor, Meteorite and Atmosphere Smart Packages."
+  summary: "The simplest no-conflict way to use AngularJS with Meteor, Meteorite and Atmosphere Smart Packages.",
+  version: "0.2.0",
+  git: "git@github.com:Urigo/ngMeteor.git"
 });
 
 Package.on_use(function (api) {
-	// Exports the ngMeteor package scope
-	api.export('ngMeteor', 'client');
+  api.versionsFrom('METEOR@0.9.0.1');
+  // Exports the ngMeteor package scope
+  api.export('ngMeteor', 'client');
 
     api.use('jquery', 'client');
 
-	// Files to load in Client only.
-	api.add_files([
-		// Lib Files
-		'lib/angular.js',
-		'lib/angular-csp.css',
-		'lib/angular-animate.js',
-		'lib/angular-cookies.js',
-		'lib/angular-resource.js',
-		'lib/angular-route.js',
-		'lib/angular-sanitize.js',
-		'lib/angular-touch.js',
-		'lib/angular-hash-key-copier.js',
-		// Module Files
-		'modules/ngMeteor-collections.js',
-		'modules/ngMeteor-template.js',
-		// Finally load ngMeteor File
-		'ngMeteor.js'
-	], 'client');
+  // Files to load in Client only.
+  api.add_files([
+    // Lib Files
+    'lib/angular.js',
+    'lib/angular-csp.css',
+    'lib/angular-animate.js',
+    'lib/angular-cookies.js',
+    'lib/angular-resource.js',
+    'lib/angular-route.js',
+    'lib/angular-sanitize.js',
+    'lib/angular-touch.js',
+    'lib/angular-hash-key-copier.js',
+    // Module Files
+    'modules/ngMeteor-collections.js',
+    'modules/ngMeteor-template.js',
+    // Finally load ngMeteor File
+    'urigo:ngmeteor.js'
+  ], 'client');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('urigo:ngmeteor');
+  api.addFiles('urigo:ngmeteor-tests.js');
 });
---
+++ urigo:ngmeteor.js
@@ -0,0 +1,27 @@
+// Define ngMeteor and its dependencies
+ngMeteor = angular.module('ngMeteor', [
+  'ngMeteor.collections',
+  'ngMeteor.template',
+  'hashKeyCopier',
+  'ngRoute',
+  'ngTouch',
+  'ngAnimate',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize'
+]);
+
+// Change the data-bindings from {{foo}} to [[foo]]
+ngMeteor.config(['$interpolateProvider',
+  function ($interpolateProvider) {
+    $interpolateProvider.startSymbol('[[');
+    $interpolateProvider.endSymbol(']]');
+  }
+]);
+
+// Manual initilisation of ngMeteor
+angular.element(document).ready(function() {
+  if (!angular.element(document).injector()){
+    angular.bootstrap(document, ['ngMeteor']);
+  }
+});
