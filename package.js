cf43a49d0948ca926c8f2fc21a85f6d6fb2075ca
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-09-07 12:34:17 +0200

    Fix path to readme

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "The simplest no-conflict way to use AngularJS with Meteor, Meteorite and Atmosphere Smart Packages.",
   version: "0.2.0",
-  git: "git@github.com:Urigo/ngMeteor.git"
+  git: "https://github.com:Urigo/ngMeteor.git"
 });
 
 Package.on_use(function (api) {
