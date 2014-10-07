83605ce13a11251d6d263621216ee34a223de168
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-10-07 21:07:30 +0300

    Rename repository

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "The simplest no-conflict way to use AngularJS with Meteor, Meteorite and Atmosphere Smart Packages.",
   version: "0.3.5",
-  git: "https://github.com/Urigo/ngMeteor.git"
+  git: "https://github.com/Urigo/angular-meteor.git"
 });
 
 Package.on_use(function (api) {
