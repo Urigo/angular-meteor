c8d8f5256ebff29be96975f5aee15b5c834cc6ca
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-10-18 21:54:59 +0300

    Fix the new user $rootScope undefined bug, thanks to @idanwe

--- modules/angular-meteor-user.js
+++ modules/angular-meteor-user.js
@@ -2,11 +2,13 @@ var angularMeteorUser = angular.module('angular-meteor.user', []);
 
 angularMeteorUser.run(['$rootScope', function($rootScope){
   Deps.autorun(function(self) {
-    $rootScope.currentUser = Meteor.user();
-    $rootScope.loggingIn = Meteor.loggingIn();
-    if (!$rootScope.$$phase) $rootScope.$apply();
-    $rootScope.$on('$destroy', function () {
-      self.stop(); // Stop computation if scope is destroyed.
-    });
+    if (Meteor.user) {
+      $rootScope.currentUser = Meteor.user();
+      $rootScope.loggingIn = Meteor.loggingIn();
+      if (!$rootScope.$$phase) $rootScope.$apply();
+      $rootScope.$on('$destroy', function () {
+        self.stop(); // Stop computation if scope is destroyed.
+      });
+    }
   });
 }]);
