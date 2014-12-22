26bb1e98afee56e5dab2b37e82a4f03723378c00
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-12-22 20:34:47 +0200

    Bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "The simplest no-conflict way to use AngularJS with Meteor, Meteorite and Atmosphere Smart Packages.",
-  version: "0.5.10",
+  version: "0.5.11",
   git: "https://github.com/Urigo/angular-meteor.git"
 });
 
