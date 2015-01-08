83f335bd560e1f6da22e325b12402f253e92e9fc
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-01-09 00:16:27 +0200

    Add $meteorObject save and reset functions, add support for multiple instances

--- modules/angular-meteor-object.js
+++ modules/angular-meteor-object.js
@@ -1,5 +1,46 @@
 var angularMeteorObject = angular.module('angular-meteor.object', []);
 
+var AngularMeteorObject = function (collection, id, options) {
+  var self = collection.findOne(id, options);
+
+  if (!self){
+    self = {};
+  }
+
+  self.__proto__ = AngularMeteorObject.prototype;
+  self.$$collection = collection;
+  self.$$options = options;
+
+  return self;
+};
+
+AngularMeteorObject.prototype = {};
+
+AngularMeteorObject.prototype.save = function save() {
+  var self = this,
+    collection = self.$$collection;
+
+  if (self)
+    if (self._id)
+      collection.update({_id: self._id}, { $set: _.omit(self, '_id', 'save', 'reset', '$$collection', '$$options') });
+};
+
+AngularMeteorObject.prototype.reset = function reset() {
+  var self = this,
+    collection = self.$$collection,
+    options = self.$$options;
+
+  if (collection){
+    var serverValue = collection.findOne(self._id, options);
+    for (var prop in serverValue) {
+      if (serverValue.hasOwnProperty(prop)) {
+        self[prop] = serverValue[prop];
+      }
+    }
+  }
+};
+
+
 angularMeteorObject.factory('$meteorObject', ['$rootScope', '$meteorUtils',
   function($rootScope, $meteorUtils) {
     return function(collection, id, auto, options) {
@@ -13,19 +54,21 @@ angularMeteorObject.factory('$meteorObject', ['$rootScope', '$meteorUtils',
         throw new TypeError("The third argument of bind must be a boolean.");
       }
 
-      var data = collection.findOne(id, options);
+      var data = new AngularMeteorObject(collection, id, options);
 
       $meteorUtils.autorun($rootScope, function() {
-        angular.copy(collection.findOne(id, options), data);
+        data.reset();
       });
 
       if (auto) { // Deep watches the model and performs autobind.
         $rootScope.$watch(function(){
-          return data;
+          return _.omit(data, 'save', 'reset', '$$collection', '$$options');
         }, function (newItem, oldItem) {
-          if (newItem)
-            if (newItem._id)
-              collection.update({_id: newItem._id}, { $set: _.omit(newItem, '_id') });
+          if (newItem) {
+            if (newItem._id) {
+              collection.update({_id: newItem._id}, {$set: _.omit(newItem, '_id', 'save', 'reset', '$$collection', '$$options')});
+            }
+          }
         }, true);
       }
 
