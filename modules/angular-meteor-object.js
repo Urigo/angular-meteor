459b79686d7b466ea9a70aa1e4091a08fe89396a
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-01-09 19:06:11 +0200

    ($meteorObject): Add subscribe method and better handle updating from null results

--- modules/angular-meteor-object.js
+++ modules/angular-meteor-object.js
@@ -1,6 +1,6 @@
 var angularMeteorObject = angular.module('angular-meteor.object', []);
 
-var AngularMeteorObject = function (collection, id, options) {
+var AngularMeteorObject = function (collection, id, options, $meteorSubscribe) {
   var self = collection.findOne(id, options);
 
   if (!self){
@@ -10,11 +10,19 @@ var AngularMeteorObject = function (collection, id, options) {
   self.__proto__ = AngularMeteorObject.prototype;
   self.$$collection = collection;
   self.$$options = options;
+  self.$$id = id;
+  self.__proto__.$meteorSubscribe = $meteorSubscribe;
 
   return self;
 };
 
 AngularMeteorObject.prototype = {};
+
+AngularMeteorObject.prototype.subscribe = function () {
+  var self = this;
+  self.$meteorSubscribe.subscribe.apply(this, arguments);
+  return this;
+};
 
 AngularMeteorObject.prototype.save = function save() {
   var self = this,
@@ -22,16 +30,17 @@ AngularMeteorObject.prototype.save = function save() {
 
   if (self)
     if (self._id)
-      collection.update({_id: self._id}, { $set: _.omit(self, '_id', 'save', 'reset', '$$collection', '$$options') });
+      collection.update({_id: self._id}, { $set: _.omit(self, '_id', 'save', 'reset', '$$collection', '$$options', '$meteorSubscribe', '$$id') });
 };
 
 AngularMeteorObject.prototype.reset = function reset() {
   var self = this,
     collection = self.$$collection,
-    options = self.$$options;
+    options = self.$$options,
+    id = self.$$id;
 
   if (collection){
-    var serverValue = collection.findOne(self._id, options);
+    var serverValue = collection.findOne(id, options);
     for (var prop in serverValue) {
       if (serverValue.hasOwnProperty(prop)) {
         self[prop] = serverValue[prop];
@@ -41,8 +50,8 @@ AngularMeteorObject.prototype.reset = function reset() {
 };
 
 
-angularMeteorObject.factory('$meteorObject', ['$rootScope', '$meteorUtils',
-  function($rootScope, $meteorUtils) {
+angularMeteorObject.factory('$meteorObject', ['$rootScope', '$meteorUtils', '$meteorSubscribe',
+  function($rootScope, $meteorUtils, $meteorSubscribe) {
     return function(collection, id, auto, options) {
 
       // Validate parameters
@@ -54,7 +63,7 @@ angularMeteorObject.factory('$meteorObject', ['$rootScope', '$meteorUtils',
         throw new TypeError("The third argument of bind must be a boolean.");
       }
 
-      var data = new AngularMeteorObject(collection, id, options);
+      var data = new AngularMeteorObject(collection, id, options, $meteorSubscribe);
 
       $meteorUtils.autorun($rootScope, function() {
         data.reset();
@@ -62,11 +71,11 @@ angularMeteorObject.factory('$meteorObject', ['$rootScope', '$meteorUtils',
 
       if (auto) { // Deep watches the model and performs autobind.
         $rootScope.$watch(function(){
-          return _.omit(data, 'save', 'reset', '$$collection', '$$options');
+          return _.omit(data, 'save', 'reset', '$$collection', '$$options', '$meteorSubscribe', '$$id');
         }, function (newItem, oldItem) {
           if (newItem) {
             if (newItem._id) {
-              collection.update({_id: newItem._id}, {$set: _.omit(newItem, '_id', 'save', 'reset', '$$collection', '$$options')});
+              collection.update({_id: newItem._id}, {$set: _.omit(newItem, '_id', 'save', 'reset', '$$collection', '$$options', '$meteorSubscribe', '$$id')});
             }
           }
         }, true);
