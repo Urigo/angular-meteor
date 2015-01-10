9d0db2d3628409478759879346e7c0e6b1a132b7
Yago Ferrer <yferrer@zephyrhealth.com>
2015-01-10 09:40:50 -0800

    Format: indent_size: 2

--- test/unit/angular-meteor-utils.Spec.js
+++ test/unit/angular-meteor-utils.Spec.js
@@ -2,67 +2,71 @@
 
 var Mongo, Tracker;
 
-describe('Given the Angular Meteor Utils Service', function() {
+describe('Given the Angular Meteor Utils Service', function () {
 
-    var $meteorUtils, $scope;
+  var $meteorUtils, $scope;
+
+  beforeEach(function () {
+
+    module('angular-meteor.utils');
+    // Injecting Services to use
+    inject(function (_$meteorUtils_, _$rootScope_) {
+      $meteorUtils = _$meteorUtils_;
+      // Creates a new child scope.
+      $scope = _$rootScope_.$new();
+    });
+
+  });
+
+  describe('when using GetCollectionByName(collectionName)', function () {
 
     beforeEach(function () {
 
-        module('angular-meteor.utils');
-        // Injecting Services to use
-        inject(function (_$meteorUtils_, _$rootScope_) {
-            $meteorUtils = _$meteorUtils_;
-            // Creates a new child scope.
-            $scope = _$rootScope_.$new();
-        });
+      // Mocking Collection
+      function Collection() {
+      }
+
+      Collection.prototype._name = 'myCollection';
+      Mongo = {Collection: Collection};
+      window.myCol = new Mongo.Collection;
+    });
+
+    it('should find and return a mongo collection by name', function () {
+      var output = $meteorUtils.getCollectionByName('myCollection');
+      expect(output).toEqual(window.myCol);
 
     });
 
-    describe('when using GetCollectionByName(collectionName)', function() {
+    it('should return "undefined" if it can\'t find the collection', function () {
 
-        beforeEach(function() {
+      var output = $meteorUtils.getCollectionByName('myCollectionFake');
+      expect(output).toEqual(undefined);
 
-            // Mocking Collection
-            function Collection() {}
-            Collection.prototype._name = 'myCollection';
-            Mongo = {Collection: Collection};
-            window.myCol = new Mongo.Collection;
-        });
+    })
+  });
 
-        it('should find and return a mongo collection by name', function() {
-            var output = $meteorUtils.getCollectionByName('myCollection');
-            expect(output).toEqual(window.myCol);
+  describe('when using autorun(scope, function)', function () {
 
-        });
+    beforeEach(function () {
 
-        it('should return "undefined" if it can\'t find the collection', function() {
-
-            var output = $meteorUtils.getCollectionByName('myCollectionFake');
-            expect(output).toEqual(undefined);
-
-        })
+      // Mocking Tracker behavior
+      Tracker = {
+        autorun: function (fn) {
+          return 'autorun';
+        }
+      }
     });
 
-    describe('when using autorun(scope, function)', function() {
+    it('should return the "autorun" object so that can be stopped manually', function () {
 
-        beforeEach(function() {
+      function fn() {
+      }
 
-            // Mocking Tracker behavior
-            Tracker = {
-                autorun: function(fn) {
-                    return 'autorun';
-                }
-            }
-        });
-
-        it('should return the "autorun" object so that can be stopped manually', function() {
-
-            function fn(){}
-            var output = $meteorUtils.autorun($scope, fn);
-            expect(output).toEqual('autorun');
-
-        });
+      var output = $meteorUtils.autorun($scope, fn);
+      expect(output).toEqual('autorun');
 
     });
+
+  });
 
 });
