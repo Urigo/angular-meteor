d029fde12c885e90d7e39cef1113f3ef37398c8d
Yago Ferrer <yferrer@zephyrhealth.com>
2015-01-10 08:51:45 -0800

    Testing: Adds code coverage support.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -30,14 +30,24 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'modules/**/*.js': 'coverage'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'dots'],
+    reporters: ['progress', 'dots', 'coverage'],
 
+
+    coverageReporter: {
+      // specify a common output directory
+      dir: '.coverage',
+      reporters: [
+        { type: 'text'}
+
+      ]
+    },
 
     // web server port
     port: 9876,
