cc0ae48f89f6b2730808f8cc4cc8dd09aa69a2ad
Yago Ferrer <yferrer@zephyrhealth.com>
2015-01-10 21:20:39 -0800

    Adds: test for $collection.bind passing a wrong type 3d parameter

--- test/unit/angular-meteor-collections.Spec.js
+++ test/unit/angular-meteor-collections.Spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var Meteor;
+var Meteor, Tracker;
 
 describe('Given the $collection Factory', function() {
 
@@ -13,6 +13,10 @@ describe('Given the $collection Factory', function() {
         Collection: function(){}
       };
 
+      Tracker = {
+        autorun: function(){}
+      };
+
       module('angular-meteor.collections');
 
       inject(function(_$collection_) {
@@ -23,11 +27,38 @@ describe('Given the $collection Factory', function() {
 
   });
 
-  it('should throw an exception if the collection passed is not an instance of Meteor.Collection', function() {
+  describe('when instantiating the collection', function() {
 
-    expect($collection.bind(function(){})).toThrow(new TypeError("The first argument of $collection must be a Meteor.Collection object."));
+    it('should throw an exception if the collection passed is not an instance of Meteor.Collection', function() {
+
+      expect($collection.bind(function(){})).toThrow(new TypeError("The first argument of $collection must be a Meteor.Collection object."));
+
+    });
+
+    it('should return an object with two methods: bind and bindOne ', function() {
+
+      var output = $collection(new Meteor.Collection());
+
+      expect(typeof output.bind).toEqual('function');
+      expect(typeof output.bindOne).toEqual('function');
+
+    });
 
   });
+
+  describe('when using $collection.bind()', function() {
+
+    it('should throw an exception in the 3rd parameter is not a boolean', function(){
+
+      var output = $collection(new Meteor.Collection());
+
+      expect(output.bind.bind(this, false, false, 'notBoolean')).toThrow(new TypeError('The third argument of bind must be a boolean.'));
+
+    });
+
+  });
+
+
 
 
 });
