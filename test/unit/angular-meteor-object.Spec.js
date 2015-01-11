69fa9987056bb81fe25d75711bd3b1804c423b30
Yago Ferrer <yferrer@zephyrhealth.com>
2015-01-11 13:32:55 -0800

    Tests: $meteorObject save()

--- test/unit/angular-meteor-object.Spec.js
+++ test/unit/angular-meteor-object.Spec.js
@@ -4,12 +4,21 @@ var Meteor;
 
 describe('Given the $meteorObject factory', function() {
 
-  var $meteorObject;
+  var $meteorObject, updateSpy;
 
   beforeEach(function() {
 
+    updateSpy = jasmine.createSpy('update');
+
+    var C = function C() {}
+
+    C.prototype.findOne = function() {
+      return {_id: 1}
+    };
+    C.prototype.update = updateSpy;
+
     Meteor = {
-      Collection: function(){}
+      Collection: C
     };
 
     module('angular-meteor.object');
@@ -20,9 +29,41 @@ describe('Given the $meteorObject factory', function() {
 
   });
 
-  it('should throw an exception if the first parameter is not an instance of Meteor.Collection', function() {
 
-    expect($meteorObject.bind(this, false)).toThrow(new TypeError("The first argument of $collection must be a Meteor.Collection object."));
+  describe('when instantiating the object', function() {
+
+    it('should throw an exception if the first parameter is not an instance of Meteor.Collection', function() {
+
+      expect($meteorObject.bind(this, false)).toThrow(new TypeError("The first argument of $collection must be a Meteor.Collection object."));
+
+    });
+
+    it('should return an AngularMeteor object that contains: save(), reset() and subscribe() methods', function() {
+
+      var myCol = new Meteor.Collection();
+      var result = $meteorObject(myCol);
+
+      expect(typeof result.save).toEqual('function');
+      expect(typeof result.reset).toEqual('function');
+      expect(typeof result.subscribe).toEqual('function');
+
+    });
+
+  });
+
+  describe('When calling save()', function() {
+
+    it('should invoke Collection.update()', function() {
+
+      var myCol = new Meteor.Collection();
+
+      var result = $meteorObject(myCol, 1);
+
+      result.save();
+
+      expect(updateSpy).toHaveBeenCalled();
+
+    });
 
   });
 
