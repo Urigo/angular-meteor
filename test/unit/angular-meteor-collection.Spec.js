f3793985901ed3b2c7f4da4c9324f8ef07899ed9
Yago Ferrer <yferrer@zephyrhealth.com>
2015-01-12 13:22:40 -0800

    Tests: Working on testing the $meteorCollection service

--- test/unit/angular-meteor-collection.Spec.js
+++ test/unit/angular-meteor-collection.Spec.js
@@ -1,15 +1,39 @@
 'use strict';
-var Mongo;
-describe('Given the $meteorCollection service', function() {
 
+var Mongo, Tracker;
+
+describe('Given the $meteorCollection service', function() {
 
   var $meteorCollection;
 
   beforeEach(function() {
 
+    var Collection = function(){};
+    Collection.prototype.find = function(){
+      return {
+        collection: {
+          name: 'myCollection'
+        },
+        observeChanges: function() {}
+      }
+    };
 
     Mongo = {
-      Collection: function(){}
+      Collection: Collection
+    };
+
+
+    Tracker = {
+      Dependency: function(){
+        return {
+          depend: function(){}
+        }
+      },
+      autorun: function (fn) {
+        fn({firstRun: false})
+      },
+      onValidate: function() {},
+      onInvalidate: function() {}
     };
 
     module('angular-meteor.meteor-collection');
@@ -27,6 +51,22 @@ describe('Given the $meteorCollection service', function() {
 
       expect($meteorCollection.bind(this)).toThrow(new TypeError("The first argument of $meteorCollection is undefined."));
       expect($meteorCollection.bind(this, 'badcollection')).toThrow(new TypeError("The first argument of $meteorCollection must be a function or a Mongo.Collection."));
+
+  });
+
+  describe('when instantiated', function() {
+
+    it('should return a Angular Meteor Collection with these methods: subscribe, save, remove, updateCursor and stop', function() {
+
+      var myCollection = new Mongo.Collection();
+      var output = $meteorCollection(myCollection);
+
+      expect(typeof output.subscribe).toEqual('function');
+      expect(typeof output.save).toEqual('function');
+      expect(typeof output.remove).toEqual('function');
+      expect(typeof output.updateCursor).toEqual('function');
+      expect(typeof output.stop).toEqual('function');
+    });
 
   });
 
