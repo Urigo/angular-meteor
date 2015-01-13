8da808ba5091fd5166274b756cc0359c45a7e653
Yago Ferrer <yferrer@zephyrhealth.com>
2015-01-13 02:13:18 -0800

    Tests: for subscribe() and $$collection

--- test/unit/angular-meteor-collection.Spec.js
+++ test/unit/angular-meteor-collection.Spec.js
@@ -1,28 +1,37 @@
 'use strict';
 
-var Mongo, Tracker;
+var Mongo, Tracker, Meteor;
 
 describe('Given the $meteorCollection service', function() {
 
-  var $meteorCollection;
+  var $meteorCollection, spySubscribe;
 
   beforeEach(function() {
 
-    var Collection = function(){};
+    // Mocking Collection
+    function Collection(){};
+
     Collection.prototype.find = function(){
       return {
         collection: {
           name: 'myCollection'
         },
-        observeChanges: function() {}
+        cursor: {
+          collection: {
+            name: 'myCollection'
+          }
+        },
+        observeChanges: function(){}
       }
     };
-
+    Collection.prototype._name = 'myCollection';
     Mongo = {
       Collection: Collection
     };
 
+    window.myCollection = new Mongo.Collection();
 
+    // Mocking Tracker
     Tracker = {
       Dependency: function(){
         return {
@@ -34,6 +43,12 @@ describe('Given the $meteorCollection service', function() {
       },
       onValidate: function() {},
       onInvalidate: function() {}
+    };
+
+    spySubscribe = jasmine.createSpy('spySubscribe');
+
+    Meteor = {
+      subscribe: spySubscribe
     };
 
     module('angular-meteor.meteor-collection');
@@ -66,6 +81,29 @@ describe('Given the $meteorCollection service', function() {
       expect(typeof output.remove).toEqual('function');
       expect(typeof output.updateCursor).toEqual('function');
       expect(typeof output.stop).toEqual('function');
+    });
+
+    it('should return a Mongo Collection inside $$collection', function(){
+
+      var myCollection = new Mongo.Collection();
+      var output = $meteorCollection(myCollection);
+      expect(typeof output.$$collection).toEqual('object');
+      expect(output.$$collection._name).toEqual('myCollection');
+
+
+    });
+
+    describe('when using subscribe()', function() {
+
+      it('should subscribe to the collection', function() {
+        var myCollection = new Mongo.Collection();
+        var output1 = $meteorCollection(myCollection);
+        output1.subscribe();
+        expect(typeof output1).toBe('object');
+        expect(spySubscribe).toHaveBeenCalled();
+
+      });
+
     });
 
   });
