6d4967b725e8f118cbd5a1432baf57d0923a38ff
Yago Ferrer <yferrer@zephyrhealth.com>
2015-01-14 11:48:01 -0800

    build: Adds coveralls

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -40,15 +40,16 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
+    reporters: ['progress', 'coverage', 'coveralls'],
 
 
     coverageReporter: {
       // specify a common output directory
-      dir: '.coverage',
+      dir: 'coverage',
       reporters: [
-        { type: 'text'},
-        { type: 'html'}
+        //{ type: 'text'},
+        //{ type: 'html'}
+        {type: 'lcov'}
       ]
     },
 
