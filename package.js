9b1496f5bb1751939a0933c10b1520e584dfabaa
Yago Ferrer <yferrer@zephyrhealth.com>
2015-01-14 21:47:06 -0800

    Build: updated test file path

--- package.js
+++ package.js
@@ -57,6 +57,6 @@ Package.onTest(function(api) {
     'modules/angular-meteor-template.js',
     'modules/angular-meteor-user.js',
     'modules/angular-meteor-utils.js',
-    'test/e2e/tests.js'
+    'test/meteor/tests.js'
   ], ['client']);
 });
