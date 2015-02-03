f6372b112561ba4fba71592124679b716e5a4cee
Fredrik Ekelund <bohemenian@gmail.com>
2015-02-03 13:18:23 +0100

    Abstracted diffArray to separate module, added tests
    Using Meteor's Tinytest to do something a lot closer to an end to
    end test than just testing the object diffing functionality.

--- test/meteor/tests.js
+++ test/meteor/tests.js
@@ -1,10 +1,9 @@
 /*
- * How to run these tests
- * go to the main directory and run:
+ * Execute these tests by going to the main directory and running:
  * meteor test-packages ./
  */
 
-Tinytest.add('When bootstrapping Angular.js ', function (test) {
+Tinytest.add('Bootstrap Angular.js ', function (test) {
 
   var called = false;
 
@@ -16,7 +15,41 @@ Tinytest.add('When bootstrapping Angular.js ', function (test) {
   };
 
   var app = angular.module('app', ['angular-meteor']);
-  angular.bootstrap(document, ['app']);
+  angular.bootstrap(document.createElement('div'), ['app']);
   test.equal(called, true, 'it should call Router.onAfterAction()');
+
+});
+
+Tinytest.addAsync('Diff changes to array', function (test, done) {
+
+  var diffArrayTest = angular.module('diffArrayTest', ['diffArray'])
+  .run(['diffArray', function (diffArray) {
+
+    var addedDoc = {_id: "c", b: 1};
+    var oldCollection = [
+      {_id: "a", identical: "property"},
+      {_id: "b", first: 2, second: {firstNested: "b"}, willBeRemoved: ":'("}
+    ];
+    var newCollection = [
+      {_id: "a", identical: "property"},
+      {_id: "b", first: 2, second: {nestedInSecond: "a"}, third: "hello"},
+      addedDoc
+    ];
+
+    diffArray(oldCollection, newCollection, {
+      addedAt: function (id, doc, pos) {
+        test.equal(doc, addedDoc);
+        test.equal(pos, 2);
+      },
+      changedAt: function (id, setDiff, unsetDiff, pos) {
+        test.equal(setDiff, {_id: "b", "second.nestedInSecond": "a", "third": "hello"});
+        test.equal(unsetDiff, {_id: "b", "second.firstNested": true, willBeRemoved: true});
+        test.equal(pos, 1);
+        done();
+      }
+    });
+  }]);
+
+  angular.bootstrap(document.createElement('div'), ['diffArrayTest']);
 
 });
