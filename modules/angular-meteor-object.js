Urigo/angular-meteor

commit c8ff4288a506c7e999a788dd82279588a8ad17f3

Refactoring AngularMeteorObject avoid triggering update if the object is empty

modules/angular-meteor-object.js
@@ -109,8 +109,8 @@ angularMeteorObject.factory('$meteorObject', ['$rootScope', '$meteorUtils', '$me
           return _.omit(data, data.$$internalProps);
         }, function (newItem, oldItem) {
           if (newItem) {
-            if (newItem._id) {
-              collection.update({_id: newItem._id}, {$set: _.omit(angular.copy(newItem), '_id')});
+            if (newItem._id && !_.isEmpty(newItem = _.omit(angular.copy(newItem), '_id'))) {
+              collection.update({_id: newItem._id}, {$set: newItem});
             }
           }
         }, true);

commit 1166f96aa0426dc0d136a5bbe7f69414a23824ad

Fix problem with autobind $meteorObject introduced in c8ff428
The _id of the Document was cleared with the rest of the properties that are omit, which is wrong and this was causing Meteor/Mongo to now know which Document should be updated, so it updated nothing

modules/angular-meteor-object.js
@@ -115,9 +115,12 @@ angularMeteorObject.factory('$meteorObject', ['$rootScope', '$meteorUtils', '$me
         data.unregisterAutoBind = $rootScope.$watch(function(){
           return _.omit(data, data.$$internalProps);
         }, function (newItem, oldItem) {
-          if (newItem)
-            if (newItem._id)
-              collection.update({_id: newItem._id}, {$set: _.omit(angular.copy(newItem), '_id')});
+          if (newItem) {
+            var newItemId = newItem._id;
+            if (newItemId && !_.isEmpty(newItem = _.omit(angular.copy(newItem), '_id'))) {
+              collection.update({_id: newItemId}, {$set: newItem});
+            }
+          }
         }, true);
       }
 