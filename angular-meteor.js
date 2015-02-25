ea895a97ccb97e83d5d93246f9f54fca0d1227de
barbatus <alex.borodach@gmail.com>
2015-02-25 21:46:20 +0300

    Update angular-meteor.js
    Adds explaining comments.

--- angular-meteor.js
+++ angular-meteor.js
@@ -17,16 +17,24 @@ angularMeteor.run(['$compile', '$document', '$rootScope', function ($compile, $d
     // Recompile after iron:router builds page
     if(typeof Router != 'undefined') {
       Router.onAfterAction(function(req, res, next) {
+        // Since Router.current().ready() is reactive we wrap it in Tracker.nonreactive to avoid re-runs.
         Tracker.nonreactive(function() {
           if (Router.current().ready()) {
+            // Again since Router.current().ready() is reactive we cant wrap it in Tracker.afterFlush
+            // therefor we use it twice for ready and not ready cases (see below).
             Tracker.afterFlush(function() {
+                // Checks if document's been compiled to the moment which happens when
+                // no loading template has been used and route is ready instantly (no waitOn in the route).
                 if (!Router.current()._docCompiled) {
                   $compile($document)($rootScope);
                   Router.current()._docCompiled = true;
                 }
+                // Document should have been compiled to that moment in all cases which means it's
+                // enough to apply scope to make it work (even if new expressions appeared there).
                 if (!$rootScope.$$phase) $rootScope.$apply();  
             });
           } else {
+            // Compiles and applies scope for the first time when current route is not ready.
             Tracker.afterFlush(function() {
               $compile($document)($rootScope);
               if (!$rootScope.$$phase) $rootScope.$apply();
