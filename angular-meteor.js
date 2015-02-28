36417ca33444c0f3824a86fc57c134a33b160a65
barbatus <alex.borodach@gmail.com>
2015-02-28 19:09:31 +0300

    Update angular-meteor.js
    Indentations

--- angular-meteor.js
+++ angular-meteor.js
@@ -21,37 +21,37 @@ angularMeteor.run(['$compile', '$document', '$rootScope', function ($compile, $d
           Tracker.nonreactive(function() {
             Tracker.afterFlush(function() {
               if (Router.current().ready()) {
-                // Since onAfterAction runs always twice when a route has waitOn's subscriptions,
-                // we need to handle case when data is already loaded at the moment Tracker.afterFlush executes
-                // which means it will run twice with Router.current().ready equals true.
-                // That's we save state to an additional auxiliry variable _done.
-                if (!Router.current()._done) {
-                    // Checks if document's been compiled to the moment.
-                    // If yes, compile only newly inserted parts.
-		                if (Router.current()._docCompiled) {
-		                    for (var prop in Router.current()._layout._regions) {
-			                    var region = Router.current()._layout._regions[prop];
-			                    var firstNode = region.view._domrange.firstNode();
-			                    var lastNode = region.view._domrange.lastNode();
-			                    $compile(firstNode)($rootScope);
-			                    if (firstNode != lastNode) {
-			                      $compile(lastNode)($rootScope);
-			                    }
-		                  	}
-		             	  } else {
-		                	$compile($document)($rootScope);
-		                  Router.current()._docCompiled = true;
-		                }
-		                if (!$rootScope.$$phase) $rootScope.$apply();
-		                Router.current()._done = true;  
-	            	}
-	          } else {
-	              // Compiles and applies scope for the first time when current route is not ready.
-              	$compile($document)($rootScope);
-              	if (!$rootScope.$$phase) $rootScope.$apply();
-	              Router.current()._docCompiled = true;
+		   // Since onAfterAction runs always twice when a route has waitOn's subscriptions,
+		   // we need to handle case when data is already loaded at the moment Tracker.afterFlush executes
+		   // which means it will run twice with Router.current().ready equals true.
+		   // That's we save state to an additional auxiliry variable _done.
+		  if (!Router.current()._done) {
+                    	// Checks if document's been compiled to the moment.
+                    	// If yes, compile only newly inserted parts.
+			if (Router.current()._docCompiled) {
+			    for (var prop in Router.current()._layout._regions) {
+			            var region = Router.current()._layout._regions[prop];
+			            var firstNode = region.view._domrange.firstNode();
+			            var lastNode = region.view._domrange.lastNode();
+			            $compile(firstNode)($rootScope);
+			            if (firstNode != lastNode) {
+			              $compile(lastNode)($rootScope);
+			            }
+			  	}
+			       } else {
+				$compile($document)($rootScope);
+			  Router.current()._docCompiled = true;
+			}
+			if (!$rootScope.$$phase) $rootScope.$apply();
+			Router.current()._done = true;  
 	          }
-          });
+	       } else {
+	           // Compiles and applies scope for the first time when current route is not ready.
+	       	   $compile($document)($rootScope);
+              	   if (!$rootScope.$$phase) $rootScope.$apply();
+	           Router.current()._docCompiled = true;
+	       }
+            });
         });
       });
   }]);
