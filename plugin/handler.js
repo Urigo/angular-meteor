Urigo/angular-meteor

commit a47ecc2213f39e5f9b0c805b10196eb6b505ccd9

Preserve comments in minified templates
Angular directives can be included with HTML comments, so we want
to preserve those in the minified templates.

plugin/handler.js
@@ -18,10 +18,9 @@ Plugin.registerSourceHandler('ng.html', {
     '$templateCache.put(\'' + compileStep.inputPath.replace(/\\/g, "/") + '\', \'' +
       minify(contents.replace(/'/g, "\\'"), {
         collapseWhitespace : true,
-        conservativeCollapse: true,
-        removeComments : true,
+        conservativeCollapse : true,
         minifyJS : true,
-        minifyCSS: true,
+        minifyCSS : true,
         processScripts : ['text/ng-template']
       }) + '\');' +
     '}]);';

commit a776386f62e4b5ae17466b236f66f73b495d5fd9

Fix problem with multi-line comments breaking compileAngularTemplates step due to invalid JS generated for the template
The issue was introduced in a47ecc2. When a ng.html file contains multi-line comments Meteor failed on the console with error like "Unexpected token ILLEGAL" due to the generated JS that adds the html to the template cache was broken.
For example this could break your app:
```
<div>
<!--
  <div>
      Containing comments on multiple lines breaks the build
  </div>
-->
</div>
```
In order to properly fix that - the best future proof approach is to JSON.stringify the strings as this ensures any JS special character is properly escaped and turned into string literal.

plugin/handler.js
@@ -15,14 +15,14 @@ Plugin.registerSourceHandler('ng.html', {
     // Ticket here: https://github.com/Urigo/angular-meteor/issues/169
     // A standardized solution to this problem might be on its way, see this ticket:
     // https://github.com/meteor/windows-preview/issues/47
-    '$templateCache.put(\'' + compileStep.inputPath.replace(/\\/g, "/") + '\', \'' +
-      minify(contents.replace(/'/g, "\\'"), {
+    '$templateCache.put(' + JSON.stringify(compileStep.inputPath.replace(/\\/g, "/")) + ', ' +
+      JSON.stringify(minify(contents, {
         collapseWhitespace : true,
         conservativeCollapse : true,
         minifyJS : true,
         minifyCSS : true,
         processScripts : ['text/ng-template']
-      }) + '\');' +
+      })) + ');' +
     '}]);';
 
   compileStep.addJavaScript({