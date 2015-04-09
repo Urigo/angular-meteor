8e6b946c47dd9ae42872e718af42a12c7d214703
Netanel Gilad <netanelgilad@gmail.com>
2015-04-09 15:02:02 +0300

    fix for #276. moved AngularMeteorObject to service, and added prototype support.

--- modules/angular-meteor-object.js
+++ modules/angular-meteor-object.js
@@ -1,110 +1,112 @@
 var angularMeteorObject = angular.module('angular-meteor.object', ['angular-meteor.utils', 'angular-meteor.subscribe']);
 
-var AngularMeteorObject = function (collection, id, options, $meteorSubscribe, $q) {
-  var self = collection.findOne(id, options);
+angularMeteorObject.factory('AngularMeteorObject', ['$q', '$meteorSubscribe', function($q, $meteorSubscribe) {
+  var AngularMeteorObject = function (collection, id, options) {
+    var self = collection.findOne(id, options);
 
-  if (!self){
-    self = {};
-  }
+    if (!self){
+      self = {};
+    }
 
-  self.__proto__ = AngularMeteorObject.prototype;
-  self.$$collection = collection;
-  self.$$options = options;
-  self.$$id = id;
-  self.__proto__.$q = $q;
-  self.__proto__.$meteorSubscribe = $meteorSubscribe;
+    self.__proto__ = AngularMeteorObject.prototype;
+    self.$$collection = collection;
+    self.$$options = options;
+    self.$$id = id;
 
-  return self;
-};
+    return self;
+  };
 
-AngularMeteorObject.prototype = {};
+  AngularMeteorObject.prototype = {};
 
-AngularMeteorObject.prototype.getRawObject = function () {
-  var self = this;
+  AngularMeteorObject.prototype.getRawObject = function () {
+    var self = this;
 
-  return angular.copy(_.omit(self, self.$$internalProps));
-};
+    return angular.copy(_.omit(self, self.$$internalProps));
+  };
 
-AngularMeteorObject.prototype.subscribe = function () {
-  var self = this;
-  self.$meteorSubscribe.subscribe.apply(this, arguments);
-  return this;
-};
+  AngularMeteorObject.prototype.subscribe = function () {
+    $meteorSubscribe.subscribe.apply(this, arguments);
+    return this;
+  };
 
-AngularMeteorObject.prototype.save = function save(docs) {
-  var self = this,
-    collection = self.$$collection,
-    $q = self.$q;
+  AngularMeteorObject.prototype.save = function save(docs) {
+    var self = this,
+      collection = self.$$collection;
 
-  var deferred = $q.defer();
+    var deferred = $q.defer();
 
-  if (self)
-    if (self._id){
-      var updates = docs? docs : angular.copy(_.omit(self, '_id', self.$$internalProps));
-      collection.update(
-        {_id: self._id},
-        { $set: updates },
-        function(error, numberOfDocs){
-          if (error) {
-            deferred.reject(error);
-          } else {
-            deferred.resolve(numberOfDocs);
+    if (self)
+      if (self._id){
+        var updates = docs? docs : angular.copy(_.omit(self, '_id', self.$$internalProps));
+        collection.update(
+          {_id: self._id},
+          { $set: updates },
+          function(error, numberOfDocs){
+            if (error) {
+              deferred.reject(error);
+            } else {
+              deferred.resolve(numberOfDocs);
+            }
+          }
+        );
+      }
+
+    return deferred.promise;
+  };
+
+  AngularMeteorObject.prototype.reset = function reset() {
+    var self = this,
+      collection = self.$$collection,
+      options = self.$$options,
+      id = self.$$id;
+
+    if (collection){
+      var serverValue = collection.findOne(id, options);
+      var prop;
+      if (serverValue) {
+        angular.extend(Object.getPrototypeOf(serverValue), AngularMeteorObject.prototype);
+        self.__proto__ = Object.getPrototypeOf(serverValue);
+        for (prop in serverValue) {
+          if (serverValue.hasOwnProperty(prop)) {
+            self[prop] = serverValue[prop];
           }
         }
-      );
-    }
-
-  return deferred.promise;
-};
-
-AngularMeteorObject.prototype.reset = function reset() {
-  var self = this,
-    collection = self.$$collection,
-    options = self.$$options,
-    id = self.$$id;
-
-  if (collection){
-    var serverValue = collection.findOne(id, options);
-    var prop;
-    if (serverValue) {
-      for (prop in serverValue) {
-        if (serverValue.hasOwnProperty(prop)) {
-          self[prop] = serverValue[prop];
+      } else {
+        for (prop in _.omit(self, self.$$internalProps)) {
+          delete self[prop];
         }
       }
-    } else {
-      for (prop in _.omit(self, self.$$internalProps)) {
-        delete self[prop];
-      }
     }
-  }
-};
+  };
 
-AngularMeteorObject.prototype.stop = function stop() {
-  if (this.unregisterAutoDestroy) {
-    this.unregisterAutoDestroy();
-  }
-  this.unregisterAutoDestroy = null;
+  AngularMeteorObject.prototype.stop = function stop() {
+    if (this.unregisterAutoDestroy) {
+      this.unregisterAutoDestroy();
+    }
+    this.unregisterAutoDestroy = null;
 
-  if (this.unregisterAutoBind) {
-    this.unregisterAutoBind();
-  }
-  this.unregisterAutoBind = null;
+    if (this.unregisterAutoBind) {
+      this.unregisterAutoBind();
+    }
+    this.unregisterAutoBind = null;
 
-  if (this.autorunComputation && this.autorunComputation.stop) {
-    this.autorunComputation.stop();
-  }
-  this.autorunComputation = null;
-};
+    if (this.autorunComputation && this.autorunComputation.stop) {
+      this.autorunComputation.stop();
+    }
+    this.autorunComputation = null;
+  };
 
 // A list of internals properties to not watch for, nor pass to the Document on update and etc.
-AngularMeteorObject.prototype.$$internalProps = [
-  'save', 'reset', '$$collection', '$$options', '$meteorSubscribe', '$$id', '$q', '$$hashkey', '$$internalProps', 'subscribe', 'stop', 'autorunComputation', 'unregisterAutoBind', 'unregisterAutoDestroy', 'getRawObject'
-];
+  AngularMeteorObject.prototype.$$internalProps = [
+    'save', 'reset', '$$collection', '$$options', '$$id', '$$hashkey', '$$internalProps', 'subscribe', 'stop', 'autorunComputation', 'unregisterAutoBind', 'unregisterAutoDestroy', 'getRawObject'
+  ];
+
+  return AngularMeteorObject;
+}]);
 
 
-angularMeteorObject.factory('$meteorObject', ['$rootScope', '$meteorUtils', '$meteorSubscribe', '$q',
-  function($rootScope, $meteorUtils, $meteorSubscribe, $q) {
+angularMeteorObject.factory('$meteorObject', ['$rootScope', '$meteorUtils', 'AngularMeteorObject',
+  function($rootScope, $meteorUtils, AngularMeteorObject) {
     return function(collection, id, auto, options) {
 
       // Validate parameters
@@ -113,7 +115,7 @@ angularMeteorObject.factory('$meteorObject', ['$rootScope', '$meteorUtils', '$me
       }
       auto = auto !== false; // Making auto default true - http://stackoverflow.com/a/15464208/1426570
 
-      var data = new AngularMeteorObject(collection, id, options, $meteorSubscribe, $q);
+      var data = new AngularMeteorObject(collection, id, options);
 
       data.autorunComputation = $meteorUtils.autorun($rootScope, function() {
         data.reset();
