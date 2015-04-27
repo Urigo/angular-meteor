3098346503eb475244c8e0d29ac5253c07403fd6
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-04-28 02:52:08 +0300

    (subscription): Add self destructing controller-subscriptions

--- modules/angular-meteor-subscribe.js
+++ modules/angular-meteor-subscribe.js
@@ -23,3 +23,33 @@ angularMeteorSubscribe.service('$meteorSubscribe', ['$q',
       return deferred.promise;
     };
   }]);
+
+angularMeteorSubscribe.run(['$rootScope', '$q',
+  function($rootScope, $q) {
+    angular.extend($rootScope, {
+      subscribe: function(){
+        var self = this;
+        var deferred = $q.defer();
+        var args = Array.prototype.slice.call(arguments);
+        var subscription = null;
+
+        // callbacks supplied as last argument
+        args.push({
+          onReady: function () {
+            deferred.resolve(subscription);
+          },
+          onError: function (err) {
+            deferred.reject(err);
+          }
+        });
+
+        subscription = Meteor.subscribe.apply(this, args);
+
+        self.$on('$destroy', function() {
+          subscription.stop();
+        });
+
+        return deferred.promise;
+      }
+  })
+}]);
