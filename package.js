23533b93913aa21d3d5a5f973e33c2d0bb13a232
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-04-29 16:39:31 +0300

    Publish 0.8.6, Uses angular:angular@1.3.15_1  to fix jquery weak dependency issue, fixes #314

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "urigo:angular",
   summary: "The simplest no-conflict way to use AngularJS with Meteor, Meteorite and Atmosphere Smart Packages.",
-  version: "0.8.5",
+  version: "0.8.6",
   git: "https://github.com/Urigo/angular-meteor.git"
 });
 
