86ef75e74a171b7f4f6ec7ea00b28464f3e7577d
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-05-28 20:09:23 +0300

    Release 0.8.11 to fix deep removal

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "urigo:angular",
   summary: "The simplest no-conflict way to use AngularJS with Meteor, Meteorite and Atmosphere Smart Packages.",
-  version: "0.8.10",
+  version: "0.8.11",
   git: "https://github.com/Urigo/angular-meteor.git"
 });
 
