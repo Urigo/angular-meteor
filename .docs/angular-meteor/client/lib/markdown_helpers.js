f80d6859d8dbf7cac674c9f8e940d482e14de995
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-06-03 01:33:51 +0300

    (docs): Escape curly braces in markdown , closes #388

---
+++ .docs/angular-meteor/client/lib/markdown_helpers.js
@@ -0,0 +1,11 @@
+Handlebars.registerHelper('dstache', function() {
+  return '{{';
+});
+
+Handlebars.registerHelper('tstache', function() {
+  return '{{{';
+});
+
+Handlebars.registerHelper('lt', function() {
+  return '<';
+});
