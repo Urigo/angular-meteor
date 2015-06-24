1d1cc87c12088891936bfae6d4242c4c04d2043d
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-06-24 22:51:38 +0300

    Release 0.9.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "urigo:angular",
   summary: "The simplest no-conflict way to use AngularJS with Meteor, Meteorite and Atmosphere Smart Packages.",
-  version: "0.9.0",
+  version: "0.9.1",
   git: "https://github.com/Urigo/angular-meteor.git"
 });
 
