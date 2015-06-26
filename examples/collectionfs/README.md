21dcf26e79ff3c8e1d8eaef8bde4fc6fa9ca267f
Dotan Simha <dotansimha@gmail.com>
2015-06-26 07:49:15 +0300

    Done with collectionfs tutorial and example

--- examples/collectionfs/README.md
+++
@@ -1,4 +0,0 @@
-Angular Meteor CollectionFS Example
-===========
-
-Small example that uses CollectionFS to upload images with angular-meteor.
--- examples/collectionfs/model.js
+++ examples/collectionfs/model.js
@@ -1,11 +1,6 @@
 Images = new FS.Collection("images", {
     stores: [
-        new FS.Store.GridFS("original"),
-        new FS.Store.GridFS("thumbnail", {
-            transformWrite: function(fileObj, readStream, writeStream) {
-                gm(readStream, fileObj.name()).resize('112', '112', '!').stream().pipe(writeStream);
-            }
-        })
+        new FS.Store.GridFS("original")
     ],
     filter: {
         allow: {
@@ -13,20 +8,3 @@ Images = new FS.Collection("images", {
         }
     }
 });
-
-if (Meteor.isServer) {
-    Meteor.methods({
-        setThumbnail: function(_id, size) {
-            var image = Images.findOne(_id);
-            var readStream = image.createReadStream('original');
-            var writeStream = image.createWriteStream('thumbnail');
-
-            //The following line should be avoided
-            writeStream.safeOn('stored', function() {
-                image.updatedAt(new Date(), {store: 'thumbnail'});
-            });
-
-            gm(readStream).crop(size.width, size.height, size.x, size.y).stream().pipe(writeStream);
-        }
-    });
-}
