bfcec105dbfb5a5b9f5f1d624244bbaad87118c8
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-07-23 21:01:21 +0300

    Prepare for code review

--- run_tests_ci.sh
+++
@@ -1,4 +0,0 @@
-VELOCITY_TEST_PACKAGES=1 meteor test-packages --driver-package velocity:html-reporter --velocity ./
-cd examples/collectionfs
-echo "######## Starting tests for example CollectionFS #######"
-VELOCITY_CI=1 meteor --test
--- tests/integration/test_collections.js
+++
@@ -1,9 +0,0 @@
-bigCollection = new Meteor.Collection('bigCollection');
-bigCollection.allow({
-    remove: function() {
-        return true;
-    },
-    insert: function() {
-        return true;
-    }
-});
