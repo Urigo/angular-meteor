fcb0590ff924c2dab15e4e6fd93bf6b640f2b837
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-08-01 22:53:25 +0300

    (docs): Add social widgets

--- .docs/angular-meteor/client/lib/helpers.js
+++ .docs/angular-meteor/client/lib/helpers.js
@@ -1,3 +1,20 @@
 Template.registerHelper('meteorVersion', function () {
   return METEOR_VERSION;
 });
+
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
