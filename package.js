02cccbb780e3d4590cd938c4ef0487f32da84ad1
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-09-09 21:26:24 +0300

    Bump version to 1.0.0-rc.5 (both angular and urigo:angular)

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "angular",
   summary: "Everything you need to use AngularJS in your Meteor app",
-  version: "1.0.0-rc.4",
+  version: "1.0.0-rc.5",
   git: "https://github.com/Urigo/angular-meteor.git"
 });
 
