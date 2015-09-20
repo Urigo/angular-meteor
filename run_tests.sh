2cf52da8730189ea8fc9ff071e8106d08c87fe4a
DAB0mB <emanor6@gmail.com>
2015-09-20 23:23:50 +0300

    Update velocity version

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,1 @@
-VELOCITY_TEST_PACKAGES=1 meteor --release 1.2-rc.17 test-packages --driver-package velocity:html-reporter@0.9.0-rc.3 ./
+velocity test-package ./ --release velocity:METEOR@1.2-rc.15_2 --driver-package=velocity:html-reporter@0.9.0-rc.4
--- run_tests_ci.sh
+++ run_tests_ci.sh
@@ -1,4 +1,4 @@
-VELOCITY_TEST_PACKAGES=1 meteor --release 1.2-rc.17 test-packages --driver-package velocity:html-reporter@0.9.0-rc.3 --velocity ./
+velocity test-package ./ --release velocity:METEOR@1.2-rc.15_2 --driver-package=velocity:html-reporter@0.9.0-rc.4 ./
 cd examples/collectionfs
 echo "######## Starting tests for example CollectionFS #######"
 VELOCITY_CI=1 meteor --test
