bf4ae6f0d6c7582434b77d7c0d1e567180cdb92c
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-09-27 13:53:57 +0300

    Release 1.0.0-rc.11 with added dependencies

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "angular",
   summary: "Everything you need to use AngularJS in your Meteor app",
-  version: "1.0.0-rc.10",
+  version: "1.0.0-rc.11",
   git: "https://github.com/Urigo/angular-meteor.git"
 });
 
@@ -44,6 +44,7 @@ Package.on_use(function (api) {
   }
   api.use('dburles:mongo-collection-instances@0.3.4', 'client'); // For getCollectionByName
 
+  api.use('ejson');
   api.use('tracker@1.0.7');
   api.use('underscore');
   api.use('session');
