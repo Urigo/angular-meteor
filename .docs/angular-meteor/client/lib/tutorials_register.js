2a7cfcc70fc1ef6bb3326872cffbe452c74d27de
Dotan Simha <dotansimha@gmail.com>
2015-10-15 22:48:30 +0300

    Add ionic tutorial

--- .docs/angular-meteor/client/lib/tutorials_register.js
+++ .docs/angular-meteor/client/lib/tutorials_register.js
@@ -12,3 +12,8 @@ DiffBox.registerTutorial('angular2-tutorial', {
   gitHubRepoName: 'barbatus/ng2-socially-tutorial',
   patchFilename: 'client/content/patches/angular2-tutorial.multi.patch'
 });
+
+DiffBox.registerTutorial('ionic-tutorial', {
+  gitHubRepoName: 'idanwe/ionic-cli-meteor-whatsapp-tutorial',
+  patchFilename: 'client/content/patches/ionic-tutorial.multi.patch'
+});
