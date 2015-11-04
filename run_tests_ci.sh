7e09c3bc244e2eba9163a005a008444a3e873c92
Netanel Gilad <netanelgilad@gmail.com>
2015-11-04 18:14:29 +0200

    test : fix ci tests and move urigo angular package

--- run_tests_ci.sh
+++ run_tests_ci.sh
@@ -1,5 +1,2 @@
 cd packages/angular-meteor-data
-VELOCITY_TEST_PACKAGES=1 meteor test-packages --driver-package velocity:html-reporter@0.9.0 ./
-cd ../../examples/collectionfs
-echo "######## Starting tests for example CollectionFS #######"
-VELOCITY_CI=1 meteor --test
+VELOCITY_TEST_PACKAGES=1 meteor test-packages --driver-package velocity:html-reporter --velocity ./
