c435a628e829d2d5cf92cb695dbf2d269b023f97
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-11-06 01:26:09 +0200

    Release 1.0.5

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "angular",
   summary: "Everything you need to use AngularJS in your Meteor app",
-  version: "1.0.4",
+  version: "1.0.5",
   git: "https://github.com/Urigo/angular-meteor.git"
 });
 
