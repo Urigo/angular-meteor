502dccf56a82b92f95e20705c2f001e9d99c7e48
Dotan Simha <dotansimha@gmail.com>
2015-11-24 14:53:35 +0200

    (feat) Update $reactive to support Cursor

--- packages/angular-meteor-data/modules/reactive.js
+++ packages/angular-meteor-data/modules/reactive.js
@@ -10,25 +10,71 @@ reactive.factory('$reactive', ['$rootScope', '$parse', ($rootScope, $parse) => {
       this.trackerDeps = {};
     }
 
-    properties (props) {
-      angular.forEach(props, (func, name) => {
-        this.computations.push(Tracker.autorun(() => {
-          var newValue = func();
-          if (angular.isUndefined(this.context[name])) {
-            this.context[name] = angular.copy(newValue);
+    properties(props) {
+      let isMeteorCursor = (obj) => {
+        return obj instanceof Mongo.Collection.Cursor;
+      };
+
+      let handleCursor = (cursor, name) => {
+        if (angular.isUndefined(this.context[name])) {
+          this.context[name] = [];
+        }
+
+        return cursor.observe({
+          addedAt: (doc, atIndex) => {
+            this.context[name].splice(atIndex, 0, doc);
+            this._propertyChanged(name);
+          },
+          changedAt: (doc, oldDoc, atIndex) => {
+            jsondiffpatch.patch(this.context[name][atIndex], jsondiffpatch.diff(this.context[name][atIndex], doc));
+            this._propertyChanged(name);
+          },
+          movedTo: (doc, fromIndex, toIndex) => {
+            this.context[name].splice(fromIndex, 1);
+            this.context[name].splice(toIndex, 0, doc);
+            this._propertyChanged(name);
+          },
+          removedAt: (oldDoc, atIndex) => {
+            this.context[name].splice(atIndex, 1);
+            this._propertyChanged(name);
+          }
+        });
+      };
+
+      let handleNonCursor = (data, name) => {
+        if (angular.isUndefined(this.context[name])) {
+          this.context[name] = angular.copy(data);
+        }
+        else {
+          if ((!_.isObject(data) && !_.isArray(data)) ||
+            (!_.isObject(this.context[name]) && !_.isArray(this.context[name]))) {
+            this.context[name] = data;
           }
           else {
-            if ((!_.isObject(newValue) && !_.isArray(newValue)) ||
-              (!_.isObject(this.context[name]) && !_.isArray(this.context[name]))) {
-              this.context[name] = newValue
-            }
-            else {
-              jsondiffpatch.patch(this.context[name], jsondiffpatch.diff(this.context[name], newValue));
-            }
+            jsondiffpatch.patch(this.context[name], jsondiffpatch.diff(this.context[name], data));
+          }
+        }
+      };
+
+      angular.forEach(props, (func, name) => {
+        this.computations.push(Tracker.autorun((comp) => {
+          let data = func();
+
+          if (isMeteorCursor(data)) {
+            let stoppableObservation = handleCursor(data, name);
+
+            comp.onInvalidate(() => {
+              stoppableObservation.stop();
+              this.context[name] = [];
+            });
+          }
+          else {
+            handleNonCursor(data, name);
           }
 
-          if (this.scope && !$rootScope.$$phase)
+          if (this.scope && !$rootScope.$$phase) {
             this.scope.$digest();
+          }
 
           this._propertyChanged(name);
         }));
@@ -64,18 +110,18 @@ reactive.factory('$reactive', ['$rootScope', '$parse', ($rootScope, $parse) => {
 
     _propertyChanged(propName) {
       angular.forEach(this.callbacks, (cb) => {
-        cb(propName)
+        cb(propName);
       });
     }
 
-    stop () {
+    stop() {
       angular.forEach(this.computations, (comp) => {
         comp.stop();
       });
     }
   }
 
-  return function(context, scope) {
+  return function (context, scope) {
     return new ReactiveContext(context, scope);
   };
 }]);
