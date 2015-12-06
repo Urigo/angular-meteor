bd0f7217b9f2c6d256f56d4f91061dc7ee631b75
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-12-06 09:09:37 -0800

    Release `angular-meteor-data@0.0.7-rc.3` and `angular@1.3.0-rc.3`

--- packages/angular/package.js
+++ packages/angular/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "angular",
   summary: "Everything you need to use AngularJS in your Meteor app",
-  version: "1.3.0",
+  version: "1.3.0-rc.3",
   git: "https://github.com/Urigo/angular-meteor.git",
   documentation: "../../README.md"
 });
@@ -10,7 +10,7 @@ Package.onUse(function (api) {
   api.versionsFrom('METEOR@1.2.0.1');
 
   api.imply([
-    'angular-meteor-data@0.0.8',
+    'angular-meteor-data@0.0.7-rc.3',
     'angular-templates@0.0.2',
     'pbastowski:angular-babel@1.0.6'
   ])
