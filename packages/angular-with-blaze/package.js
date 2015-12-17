7d6057759a1995931f12ef71a4c6a4ddb902fee1
Dotan Simha <dotansimha@gmail.com>
2015-12-17 18:34:18 +0200

    Bump versions

--- packages/angular-with-blaze/package.js
+++ packages/angular-with-blaze/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "angular-with-blaze",
   summary: "Everything you need to use both AngularJS and Blaze templates in your Meteor app",
-  version: "1.3.0",
+  version: "1.3.1",
   git: "https://github.com/Urigo/angular-meteor.git",
   documentation: "../../README.md"
 });
@@ -10,7 +10,7 @@ Package.onUse(function (api) {
   api.versionsFrom('METEOR@1.2.0.1');
 
   api.imply([
-    'angular-meteor-data@0.0.7',
+    'angular-meteor-data@0.0.8',
     'angular-blaze-templates-compiler@0.0.1'
   ])
 });
