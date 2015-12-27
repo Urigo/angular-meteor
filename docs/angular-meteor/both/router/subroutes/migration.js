a348710d0d051350eab9de126304973296133123
ozsayag <ozsayag@gmail.com>
2015-12-27 16:18:51 +0200

    initial migration guide

--- docs/angular-meteor/both/router/subroutes/migration.js
+++ docs/angular-meteor/both/router/subroutes/migration.js
@@ -1,20 +1,29 @@
 MIGRATION = {
   groups: [
     {
-      id: "1",
-      title: "Angular 1",
-      route: "migration.angular1",
-      path: "/migration/angular1",
+      id: "0",
+      title: "Migration",
+      route: "migration",
+      path: "/migration",
       pathRedirect: "/migration/angular1/01-intro",
-      ghRepoName: "https://github.com/ozsay/blaze2angular",
+      template: "migration",
       pages: [
         {
-          id: "10",
-          title: 'Test',
-          seoTitle: 'Test',
-          route: "migration.angular1.01-intro",
-          path: "/migration/angular1/01-intro",
-          contentTemplate: 'migration.angular1.01-intro.md'
+          id: "1",
+          title: "Angular 1",
+          route: "migration.angular1",
+          path: "/migration/angular1",
+          pathRedirect: "/migration/angular1/01-intro",
+          pages: [
+            {
+              id: "10",
+              title: 'Intro',
+              seoTitle: 'Intro',
+              route: "migration.angular1.01-intro",
+              path: "/migration/angular1/01-intro",
+              contentTemplate: 'migration.angular1.01-intro.md'
+            }
+          ]
         }
       ]
     }
