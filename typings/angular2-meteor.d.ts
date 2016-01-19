691d8d886866c8fe7c51995b6b701e9385a3b0aa
Dotan Simha <dotansimha@gmail.com>
2016-01-19 15:09:22 +0200

    - Updated tsd.json - Updated typings versions

--- typings/angular2-meteor.d.ts
+++ typings/angular2-meteor.d.ts
@@ -1,6 +1,10 @@
 
 /// <reference path="angular2.d.ts" />
 /// <reference path="meteor/meteor.d.ts" />
+/// <reference path="underscore/underscore.d.ts" />
+/// <reference path="angular2/angular2.d.ts" />
+/// <reference path="es6-promise/es6-promise.d.ts" />
+/// <reference path="es6-shim/es6-shim.d.ts" />
 
 declare module ngMeteor {
   class MeteorComponent {
