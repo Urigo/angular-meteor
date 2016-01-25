179056644c00d2678470a838347a208bba3d1185
Dotan Simha <dotansimha@gmail.com>
2016-01-25 13:46:37 +0200

    Fix install scripts

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
+var flatten = require('gulp-flatten');
 var merge = require('merge2');
 
-gulp.task('build', function () {
+gulp.task('build', ['copy'], function () {
   var tsResult =  gulp.src(['modules/**/*.ts'])
     .pipe(ts(require('./tsconfig.json').compilerOptions));
 
@@ -10,6 +11,12 @@ gulp.task('build', function () {
     tsResult.dts.pipe(gulp.dest('./')),
     tsResult.js.pipe(gulp.dest('./'))
   ]);
+});
+
+gulp.task('copy', function() {
+  return gulp.src(['modules/*.ts'])
+    .pipe(flatten())
+  .pipe(gulp.dest('./'));
 });
 
 gulp.task('default', ['build']);
