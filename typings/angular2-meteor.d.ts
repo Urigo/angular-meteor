a307ea3ae97e5d9ed3178e08e8abda080f23ac58
Dotan Simha <dotansimha@gmail.com>
2016-01-26 15:26:35 +0200

    Fix TSD file

--- typings/angular2-meteor.d.ts
+++ typings/angular2-meteor.d.ts
@@ -2,3 +2,6 @@
 /// <reference path="es6-promise/es6-promise.d.ts" />
 /// <reference path="meteor/meteor.d.ts" />
 /// <reference path="underscore/underscore.d.ts" />
+/// <reference path="../node_modules/definition-header/dist/index.d.ts" />
+/// <reference path="../node_modules/reflect-metadata/reflect-metadata.d.ts" />
+/// <reference path="es6-shim/es6-shim.d.ts" />
