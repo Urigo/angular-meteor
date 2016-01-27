ed6e248d2b313d33cb36f4300f35519608f8086f
JD Conley <jd.conley@gmailcom>
2016-01-27 10:35:23 -0800

    Add ngOnDestroy to typings to enable inheritance
    This is to support inheriting from MeteorComponent. If you extend
    MeteorComponent and don’t call this ngOnDestroy you end up leaking via
    the autoruns and subscriptions.

--- typings/angular2-meteor.d.ts
+++ typings/angular2-meteor.d.ts
@@ -7,6 +7,7 @@ declare module ngMeteor {
     subscribe(name: string, ...rest: any[]): Meteor.SubscriptionHandle;
     autorun(runFunc: Function, autoBind?: boolean): Tracker.Computation;
     call(name: string, ...rest: any[]);
+    ngOnDestroy():void;
   }
 
   function bootstrap(appComponentType: /*Type*/ any, bindings?: Array<core.Type | core.Provider | any[]>): Promise<core.ApplicationRef>;
