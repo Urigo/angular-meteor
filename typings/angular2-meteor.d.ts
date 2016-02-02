ee5e5f0505f7eba4043e3e23de988680dcbdf11d
Dotan Simha <dotansimha@gmail.com>
2016-02-02 18:41:28 +0200

    chore(typings): Cleanup the typings folder

--- typings/angular2-meteor.d.ts
+++ typings/angular2-meteor.d.ts
@@ -1,7 +1,1 @@
-/// <reference path="zone/zone.d.ts" />
-/// <reference path="es6-promise/es6-promise.d.ts" />
 /// <reference path="meteor/meteor.d.ts" />
-/// <reference path="underscore/underscore.d.ts" />
-/// <reference path="../node_modules/definition-header/dist/index.d.ts" />
-/// <reference path="../node_modules/reflect-metadata/reflect-metadata.d.ts" />
-/// <reference path="es6-shim/es6-shim.d.ts" />
