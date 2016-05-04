ee3e3eb96d3fea1f69a7bfe48b0d16b65541d2d0
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-05-04 14:09:04 +0200

    chore(templates): remove cheerio from npm dependencies

--- packages/angular-templates-runtime/package.js
+++ packages/angular-templates-runtime/package.js
@@ -8,8 +8,7 @@ Package.describe({
 
 Npm.depends({
   'html-minifier' : '0.6.9',
-  'uglify-js': '2.4.24',
-  'cheerio': '0.19.0' // why do we keep it?
+  'uglify-js': '2.4.24'
 });
 
 Package.onUse(function(api) {
