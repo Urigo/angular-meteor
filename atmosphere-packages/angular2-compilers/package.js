25eca372dd2848c8f1fc26b94af62d2a41fb36af
barbatus <alex.borodach@gmail.com>
2016-07-08 12:21:34 +0300

    chore: bump css-compiler

--- atmosphere-packages/angular2-compilers/package.js
+++ atmosphere-packages/angular2-compilers/package.js
@@ -15,7 +15,7 @@ Package.registerBuildPlugin({
     // Uses an external packages to get the actual compilers
     'ecmascript@0.4.3',
     'urigo:static-html-compiler@0.0.5',
-    'barbatus:css-compiler@0.2.0'
+    'barbatus:css-compiler@0.3.0'
   ]
 });
 
