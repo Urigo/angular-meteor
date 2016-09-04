b8d3d327843e87d29d91f075b1ebc47ba2f2dd94
Per Bernhardsson <per@goldcrestit.se>
2016-09-04 22:43:23 +0200

    chore(observable): Removed dist files from removed sources.

--- dist/minimongo-observable/observable-subscription.d.ts
+++
@@ -1,5 +0,0 @@
-import { Observable, Subscriber } from 'rxjs';
-export declare class ObservableMeteorSubscription<T> extends Observable<T> {
-    static create<T>(subscribe?: (subscriber: Subscriber<T>) => any): ObservableMeteorSubscription<T>;
-    constructor(subscribe?: (subscriber: Subscriber<T>) => any);
-}
--- dist/minimongo-observable/to-observable.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-var observable_cursor_1 = require('./observable-cursor');
-function toObservable(cursor) {
-    return observable_cursor_1.ObservableCursor.create(cursor);
-}
-exports.toObservable = toObservable;
