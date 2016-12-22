b6b0ea7b9947b90f6de44c9fb7cd016e700c1802
dotansimha <dotansimha@gmail.com>
2016-12-22 11:08:49 +0200

    chore(dependencies): add yarn support, remove typings, update dependencies

--- modules/index.ts
+++ modules/index.ts
@@ -1,7 +1,3 @@
-/// <reference types="zone.js" />
-/// <reference types="meteor-typings" />
-/// <reference types="@types/underscore" />
-
 export * from './zone_utils';
 export * from './mongo_cursor_observer';
 export * from './mongo_cursor_differ';
