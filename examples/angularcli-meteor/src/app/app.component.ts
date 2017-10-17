01c601327f883af0d37f1e5664a04b984f08030f
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-10-17 17:16:27 +0200

    Add angular-cli example

---
+++ examples/angularcli-meteor/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Chats } from '../../api/server/collections';
+import { Chat } from '../../api/server/models';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'app';
+  chats: Chat[];
+
+  constructor() {}
+
+  ngOnInit() {
+    Chats.find({}).subscribe((chats: Chat[]) => {
+      console.log(chats);
+      this.chats = chats;
+    });
+  }
+}
