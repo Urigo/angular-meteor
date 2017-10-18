d631ea01830a7ec5e26a056ba7036dd8483a0a73
Arda TANRIKULU <ardatanrikulu@gmail.com>
2017-10-18 10:16:51 -0400

    New test added for MeteorCLI compiler

---
+++ tests/angular2-compilers-tests/client/imports/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  templateUrl: 'app.html',
+  styleUrls: ['app.scss']
+})
+export class AppComponent {
+  greetingText: string = 'Hello World!';
+}
