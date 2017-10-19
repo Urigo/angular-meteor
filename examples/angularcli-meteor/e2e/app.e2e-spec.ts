79993f0864f65284afae3c209c4d63310adb9352
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-10-19 22:54:02 +0200

    Add a Meteor test plus some npm run scripts

--- examples/angularcli-meteor/e2e/app.e2e-spec.ts
+++ examples/angularcli-meteor/e2e/app.e2e-spec.ts
@@ -12,9 +12,12 @@ describe('angularcli-meteor App', () => {
     expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 
-  it('should load chats from them Meteor backend', () => {
-    const chats = '[ { "title": "Ethan Gonzalez", "picture": "https://randomuser.me/api/portraits/thumb/men/1.jpg", "_id": "YHiPouKHn33TfvJ4N" }, { "title": "Bryan Wallace", "picture": "https://randomuser.me/api/portraits/thumb/lego/1.jpg", "_id": "QoPA22itTNmux82cj" }, { "title": "Avery Stewart", "picture": "https://randomuser.me/api/portraits/thumb/women/1.jpg", "_id": "4zN8GZc7cr68PibaD" }, { "title": "Katie Peterson", "picture": "https://randomuser.me/api/portraits/thumb/women/2.jpg", "_id": "yEroDzg5wCbdgDXFJ" }, { "title": "Ray Edwards", "picture": "https://randomuser.me/api/portraits/thumb/men/2.jpg", "_id": "pWJXJHhhrDxiyczrF" } ]';
+  it('should load chats from the Meteor backend', () => {
     page.navigateTo();
-    expect(page.getLastDiv()).toEqual(chats);
+    expect(page.getLastDiv()).toContain('Ethan Gonzalez');
+    expect(page.getLastDiv()).toContain('Bryan Wallace');
+    expect(page.getLastDiv()).toContain('Avery Stewart');
+    expect(page.getLastDiv()).toContain('Katie Peterson');
+    expect(page.getLastDiv()).toContain('Ray Edwards');
   });
 });
