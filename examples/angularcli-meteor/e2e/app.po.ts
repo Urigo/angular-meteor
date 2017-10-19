e5ce3c280772c870c8a1637484aeabaef3111b93
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-10-19 15:36:06 +0200

    Fix testing for CI and add Meteor e2e test

--- examples/angularcli-meteor/e2e/app.po.ts
+++ examples/angularcli-meteor/e2e/app.po.ts
@@ -8,4 +8,8 @@ export class AppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getLastDiv() {
+    return element(by.css('app-root div:last-child')).getText();
+  }
 }
