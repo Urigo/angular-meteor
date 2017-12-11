b68a294fbb3328de2f1998e36bbbda77e1c99f74
Igor Perevozchikov <iperevozchikov@gmail.com>
2017-12-11 10:00:56 +0700

    Trim spaces

--- atmosphere-packages/angular-compilers/package.js
+++ atmosphere-packages/angular-compilers/package.js
@@ -13,7 +13,7 @@ Package.registerBuildPlugin({
   ],
   use: [
     // Uses an external packages to get the actual compilers
-    'ecmascript@0.8.3',   
+    'ecmascript@0.8.3',
     'angular-typescript-compiler@0.2.9_5',
     'angular-html-compiler@0.2.9',
     'angular-scss-compiler@0.2.9'
