1551e9b6a7f7fc0b47db9fe3c28edccc9a0662b8
Arda TANRIKULU <ardatanrikulu@gmail.com>
2018-03-18 07:33:12 -0400

    fix tests by adding google-chrome package

--- run_tests.sh
+++ run_tests.sh
@@ -12,7 +12,6 @@ cd ../../angularcli-meteor
 npm install
 npm run meteor-client:bundle
 npm run api:reset
-export CHROME_BIN=chromium-browser
 npm run test
 exit_code=$?; if [ ${exit_code} -gt 0 ]; then exit ${exit_code}; fi
 #concurrently "npm run start" "npm run api" "sleep 180; npm run e2e" --kill-others --success first
