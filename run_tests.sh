4962941842161ac3949c7ea48572ccef1c6b627d
Arda TANRIKULU <ardatanrikulu@gmail.com>
2018-03-20 09:10:12 -0400

    add aot and rollup tests

--- run_tests.sh
+++ run_tests.sh
@@ -11,7 +11,12 @@ cd ../../angularcli-meteor
 npm ci
 npm run meteor-client:bundle
 npm run api:reset
+# Testing JIT
 npm run test
+# Testing AOT w/o Rollup
+AOT=1 npm run test --production
+# Testing AOT with Rollup
+AOT=1 ROLLUP=1 npm run test --production
 exit_code=$?; if [ ${exit_code} -gt 0 ]; then exit ${exit_code}; fi
 #concurrently "npm run start" "npm run api" "sleep 180; npm run e2e" --kill-others --success first
 concurrently "npm run start" "sleep 30; npm run e2e" --kill-others --success first
