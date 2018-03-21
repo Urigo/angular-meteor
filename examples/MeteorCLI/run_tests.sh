fa52f74bcba3beafe8b68b41d6f05c93ee4e0b08
Arda TANRIKULU <ardatanrikulu@gmail.com>
2018-03-21 11:22:36 -0400

    update test scripts

--- examples/MeteorCLI/run_tests.sh
+++ examples/MeteorCLI/run_tests.sh
@@ -2,14 +2,14 @@
 export NODE_OPTIONS=--max-old-space-size=4096
 export METEOR_PACKAGE_DIRS=../../../atmosphere-packages
 cd ./bare
-./run_tests.sh
+sh run_tests.sh
 exit_code=$?; if [ ${exit_code} -gt 0 ]; then exit ${exit_code}; fi
 cd ../lazy-loading
-./run_tests.sh
+sh run_tests.sh
 exit_code=$?; if [ ${exit_code} -gt 0 ]; then exit ${exit_code}; fi
 cd ../universal
-./run_tests.sh
+sh run_tests.sh
 exit_code=$?; if [ ${exit_code} -gt 0 ]; then exit ${exit_code}; fi
 cd ../all-in-one
-./run_tests.sh
+sh run_tests.sh
 exit_code=$?; if [ ${exit_code} -gt 0 ]; then exit ${exit_code}; fi
