693cee5af4f7bcddc5b77faedbec046da2754d2f
Arda TANRIKULU <ardatanrikulu@gmail.com>
2018-03-21 11:28:25 -0400

    Update test script

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-(cd examples/MeteorCLI && ./run_tests.sh)
+(cd examples/MeteorCLI && sh run_tests.sh)
 exit_code=$?; if [ ${exit_code} -gt 0 ]; then exit ${exit_code}; fi
-(cd examples/angularjs && ./run_tests.sh)
+(cd examples/angularjs && sh run_tests.sh)
 exit_code=$?; if [ ${exit_code} -gt 0 ]; then exit ${exit_code}; fi
-(cd examples/angularcli-meteor && ./run_tests.sh)
+(cd examples/angularcli-meteor && sh run_tests.sh)
 exit_code=$?; if [ ${exit_code} -gt 0 ]; then exit ${exit_code}; fi
